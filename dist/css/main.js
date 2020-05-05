eb0d3c263d579a9e26e2b016801c8c34606a901b
Milton Bedah <miltonbedah@gmail.com>
2020-05-05 19:26:42 +0300

    Another Commit

---
+++ dist/css/main.js
@@ -0,0 +1,10 @@
+// Setting the variables and constants
+const disable = document.querySelector('#disableBtn');
+
+
+// event listener for the disable button
+
+disable.addEventListener('click', disableAnimation =>{
+    
+});
+
