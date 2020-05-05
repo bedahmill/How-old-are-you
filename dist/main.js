3531125bc4ea663002b2ef0193cd033c195c2d5d
Milton Bedah <miltonbedah@gmail.com>
2020-05-05 22:21:35 +0300

    Working on Date

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,9 @@
-
 // Setting the variables and constants
 const disable = document.querySelector('#disableBtn');
-let bodyFx= document.querySelector('.bodyAnimation')
+const input = document.getElementById('birthdayInput'); 
+const findOut = document.getElementById('explore');
 
 // event listener for the disable button
-
 disable.addEventListener('click', disableAnimation =>{
     document.getElementById('me').style.background ='#000000'; // background for old browsers
     document.getElementById('me').style.background ='-webkit-linear-gradient(to right, #000000, #434343)'; //for Chrome 10-25, Safari 5.1-6
@@ -12,7 +11,30 @@ disable.addEventListener('click', disableAnimation =>{
 
     disable.remove();
     document.getElementById('disclaimer').remove();
-    document.getElementById('enjoy').style.visibility = 'visible';
-
+    // document.getElementById('enjoy').style.visibility = 'visible';
+    const x = document.getElementById('enjoy');
+    setTimeout(function(){
+         x.style.visibility = 'visible';  // time function to display the enjoy text
+         }, 1000);    
+    setTimeout(function(){
+         x.style.visibility = 'hidden';  // time function to remove the enjoy text
+         }, 3000);    
 });
+
+// event for find out button
+findOut.addEventListener('click', find =>{
+   let date = new Date(input.value);  // changing the input value to a date function
+   let month = date.getMonth() +1;
+   let day  = date.getDate();
+   let year = date.getFullYear();
+
+
+    console.log("Day is :"+ day);
+    console.log('Month is' + month);
+    console.log('Year is' + year);
+
+    let tdate = new Date();
+    let 
+});
+
 
